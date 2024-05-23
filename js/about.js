f416887cf2e2c39640d2237ccc50d1309aae23a1
Luis <minimalmotion.contact@gmail.com>
2024-05-23 14:38:34 +0200

    ABOUT Y VIDEO COMPRIMIDO

---
+++ js/about.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const logoButton = document.getElementById('logo-button');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+
+    logoButton.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('show');
+    });
+});
