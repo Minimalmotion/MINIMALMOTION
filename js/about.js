061341e3f4a92dedf6ea336732c4e7e9e30c3528
adrianortzs <adrianortizsuarez@icloud.com>
2024-06-30 19:37:03 +0200

    menu desaparece

--- js/about.js
+++ js/about.js
@@ -5,5 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     logoButton.addEventListener('click', () => {
         dropdownMenu.classList.toggle('show');
     });
+
+    // Ocultar el menú al hacer clic fuera de él
+    document.addEventListener('click', (event) => {
+        // Si el clic fue en el botón del logo o en el menú, no hacer nada
+        if (logoButton.contains(event.target) || dropdownMenu.contains(event.target)) {
+            return;
+        }
+        // Si el menú está visible y se hace clic fuera, ocultarlo
+        if (dropdownMenu.classList.contains('show')) {
+            dropdownMenu.classList.remove('show');
+        }
+    });
 });
 
--- js/proyectos.js
+++ js/proyectos.js
@@ -7,6 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
     logoButton.addEventListener('click', () => {
         dropdownMenu.classList.toggle('show');
     });
+
+    // Ocultar el menú al hacer clic fuera de él
+    document.addEventListener('click', (event) => {
+      // Si el clic fue en el botón del logo o en el menú, no hacer nada
+      if (logoButton.contains(event.target) || dropdownMenu.contains(event.target)) {
+          return;
+      }
+      // Si el menú está visible y se hace clic fuera, ocultarlo
+      if (dropdownMenu.classList.contains('show')) {
+          dropdownMenu.classList.remove('show');
+      }
+  });
   
     //////////////////////////////////////////////////////////////////////
     function showModal(videoElement) {
